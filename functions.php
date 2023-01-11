135c4503375602bf52c815441bed717bcd62a992
LiliwoL <github@liliwol.fr>
2023-01-11 20:42:11 +0100

    Prise en charge verbes pronominaux

--- functions.php
+++ functions.php
@@ -1,7 +1,10 @@
 <?php
 
 /**
- * 
+ * Vériufie qu'il s'agit bien d'un verbe du premier groupe (qui finit en er)
+ *
+ * @param string $verb
+ * @return boolean
  */
 function checkPremierGroupe (string $verb) : bool
 {
@@ -12,25 +15,79 @@ function checkPremierGroupe (string $verb) : bool
         return false;
 }
 
-function conjuguer( string $verb) : string
+/**
+ * Vérifie que c'est un verbe pronominal ou non (qui commence par se)
+ *
+ * @param string $verb
+ * @return boolean
+ */
+function isVerbePronominal (string $verb) : bool
+{
+    // Vérifie les deux premières lettres du verbe mises en minuscule
+    if ( strtolower(substr($verb, 0, 2)) == "se" )
+        return true;
+    else
+        return false;
+}
+
+function conjuguerVerbePronominal( string $verb ) : string
 {
     // Je
-    $output = "Je " . substr($verb, 0, -1) . "\n";
+    $output = "Je me " . substr($verb, 0, -1) . "\n";
 
     // Tu
-    $output .= "Tu " . substr($verb, 0, -1) . "s" . "\n";
+    $output .= "Tu te " . substr($verb, 0, -1) . "s" . "\n";
 
     // Il Elle On
-    $output .= "Il / Elle / On " . substr($verb, 0, -1) . "\n";
+    $output .= "Il / Elle / On se " . substr($verb, 0, -1) . "\n";
 
     // Nous
-    $output .= "Nous " . substr($verb, 0, -2) . "ons" . "\n";
+    $output .= "Nous nous" . substr($verb, 0, -2) . "ons" . "\n";
 
     // Vous
-    $output .= "Vous " . substr($verb, 0, -2) . "ez" . "\n";
+    $output .= "Vous vous " . substr($verb, 0, -2) . "ez" . "\n";
 
     // Ils Elles
-    $output .= "Ils / Elles " . substr($verb, 0, -1) . "nt" . "\n";
+    $output .= "Ils / Elles se" . substr($verb, 0, -1) . "nt" . "\n";
+
+    return $output;
+}
+
+
+/**
+ * Fonction principale pour conjuguer le verbe
+ *
+ * @param string $verb
+ * @return string
+ */
+function conjuguer( string $verb) : string
+{
+
+    // Test si verbe pronominal
+    if ( isVerbePronominal($verb) )
+    {
+        $output = conjuguerVerbePronominal($verb);
+    }
+    else
+    {
+        // Je
+        $output = "Je " . substr($verb, 0, -1) . "\n";
+
+        // Tu
+        $output .= "Tu " . substr($verb, 0, -1) . "s" . "\n";
+
+        // Il Elle On
+        $output .= "Il / Elle / On " . substr($verb, 0, -1) . "\n";
+
+        // Nous
+        $output .= "Nous " . substr($verb, 0, -2) . "ons" . "\n";
+
+        // Vous
+        $output .= "Vous " . substr($verb, 0, -2) . "ez" . "\n";
+
+        // Ils Elles
+        $output .= "Ils / Elles " . substr($verb, 0, -1) . "nt" . "\n";
+    }
 
     return $output;
 }
